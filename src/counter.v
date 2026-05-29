23ab773e4cdd95daf99d0407924eb87ff910591f
Praj5-elinnos <prajwal@elinnos.com>
2026-05-29 04:58:01 +0000

    Update counter.v with latest changes

--- src/counter.v
+++ src/counter.v
@@ -1,9 +1,11 @@
+`timescale 1ns/1ps
+
 module counter(
   input clk,
   input rst,
   output reg [3:0] count
 );
-  always @(posedge clk or posedge rst)) begin
+  always @(posedge clk or posedge rst) begin
     if (rst)
       count <= 4'b0000;
     else
